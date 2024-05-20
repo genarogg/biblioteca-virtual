a9683fa435d00d7ef334703103923ba06b7439db
genaro gonzalez <genarrogg@gmail.com>
2024-05-19 20:40:10 -0400

    json server

--- src/controllers/formCarga.js
+++ src/controllers/formCarga.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Trabajo, Categoria } from "../models/index.js";
 
 const formCargaGet = (req, res) => {
@@ -23,13 +24,20 @@ const formCargaPost = async (req, res) => {
     !nombreAutor ||
     !apellidoAutor ||
     !cedulaAutor ||
-    !filename ||
-    !userAgent
+    !filename
   ) {
     return res.status(400).json({ error: "Todos los campos son necesarios" });
   }
 
   try {
+    const trabajoExistente = await Trabajo.findOne({
+      where: { rutaPDF: "/uploads/" + filename, cedulaAutor },
+    });
+
+    if (trabajoExistente) {
+      return res.status(400).json({ error: "Trabajo duplicado" });
+    }
+
     await Trabajo.create({
       categoria,
       titulo,
@@ -47,12 +55,23 @@ const formCargaPost = async (req, res) => {
 
     // Asegurarse de que exista al menos un registro en la tabla 'categorias'
     const categoriaExistente = await Categoria.findOne();
+
     if (categoriaExistente) {
       await Categoria.increment(incrementObject, { where: {} });
     } else {
       // Crear un nuevo registro en la tabla 'categorias' si no existe ninguno
       await Categoria.create(incrementObject);
     }
+
+    // Guardar la categoría en el servidor JSON
+    axios
+      .post(`http://localhost:8001/${categoria}`, {
+        categoria,
+        titulo,
+        descripcion,
+      })
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(error));
 
     res.status(200).json({ message: "Trabajo cargado exitosamente" });
   } catch (error) {
